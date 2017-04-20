0b832a79f8724049e4048e59574e56afe2acd6a0
snehasis007 <snehasis007@gmail.com>
2017-04-20 09:28:18 +0530

    T9

--- src/main/java/com/cts/cldapps/messaging/Receiver.java
+++ src/main/java/com/cts/cldapps/messaging/Receiver.java
@@ -1,15 +1,28 @@
 package com.cts.cldapps.messaging;
 
 import java.util.concurrent.CountDownLatch;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.cts.cldapps.domain.Employee;
+import com.cts.cldapps.repositories.EmployeeRepository;
 
 @Component
 public class Receiver {
 	private CountDownLatch latch = new CountDownLatch(1);
-
-    public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
-        latch.countDown();
+	private  EmployeeRepository employeeRepository;
+	@Autowired
+    public Receiver(EmployeeRepository repository) {
+        this.employeeRepository = repository;
+	}
+	
+    public void receiveMessage(Object message) {
+        if(message instanceof Employee){
+        	Employee up=(Employee)message;
+        	employeeRepository.save(up);
+        }
+        
     }
 
     public CountDownLatch getLatch() {
